217ffc727e6e2cafed74fdd98fd80d552df3f615
zhuxinyao99-jpg <zhuxinyao99@gmail.com>
2026-06-08 16:23:01 +0800

    fix: 恢复球体为原始亮橙色配色 (#ffffff/#ffbb00/#0700ff)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -230,13 +230,13 @@ export default function Home() {
       >
         <ShaderGradientCanvas style={{ position: 'absolute', inset: 0 }} pixelDensity={1.5} fov={45}>
           <ShaderGradient
-            animate="on" brightness={0.6} cAzimuthAngle={120} cDistance={7.1}
+            animate="on" brightness={1.1} cAzimuthAngle={0} cDistance={7.1}
             cPolarAngle={140} cameraZoom={17.3}
-            color1="#c2762a" color2="#f09020" color3="#b54a0a"
-            envPreset="city" grain="on" lightType="3d" reflection={0.1}
+            color1="#ffffff" color2="#ffbb00" color3="#0700ff"
+            envPreset="city" grain="off" lightType="3d" reflection={0.1}
             shader="defaults" type="sphere"
             uAmplitude={1.4} uDensity={1.1} uFrequency={5.5}
-            uSpeed={0.1} uStrength={1} uTime={8}
+            uSpeed={0.1} uStrength={1} uTime={0}
             wireframe={false} enableTransition={true}
           />
         </ShaderGradientCanvas>
